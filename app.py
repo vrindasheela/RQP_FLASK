85cb13f9c492e26a9aa0c2c1f487555368eb9715
VRINDA ASOKAN <vrinda.asokan@gmail.com>
2019-04-12 11:15:26 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import sqlite3 as sql
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,7 +9,11 @@ def home():
 @app.route('/student', methods = ['POST', 'GET'])
 def student():
     if request.method == 'POST':
-        return render_template('qp1.html')
+        code = request.form['code']
+        if code == '1':
+            return render_template('qp1.html')
+        else:
+            return render_template('qp2.html')
 
 @app.route('/success',methods = ['POST', 'GET'])
 def result():
@@ -17,6 +22,60 @@ def result():
       #ans2 = request.form['ans2']
       ans = request.form
       return render_template("success.html",ans = ans)
+
+@app.route('/enternew')
+def new_student():
+   return render_template('student1.html')
+
+@app.route('/addrec',methods = ['POST', 'GET'])
+def addrec():
+   if request.method == 'POST':
+      try:
+         nm = request.form['nm']
+         addr = request.form['add']
+         city = request.form['city']
+         pin = request.form['pin']
+         
+         with sql.connect("database.db") as con:
+            cur = con.cursor()
+            cur.execute("INSERT INTO students (name,addr,city,pin) VALUES (?,?,?,?)",(nm,addr,city,pin) )
+            
+            con.commit()
+            msg = "Record successfully added"
+      except:
+         con.rollback()
+         msg = "error in insert operation"
+      
+      finally:
+         return render_template("result1.html",msg = msg)
+         con.close()
+
+'''@app.route('/enternew')
+def new_student():
+   return render_template('student.html')'''
+
+'''@app.route('/addrec',methods = ['POST', 'GET'])
+def addrec():
+   if request.method == 'POST':
+      try:
+         nm = request.form['nm']
+         addr = request.form['add']
+         city = request.form['city']
+         pin = request.form['pin']
+         
+         with sql.connect("RQP.db") as con:
+            cur = con.cursor()
+            cur.execute("INSERT INTO students (name,addr,city,pin) VALUES (?,?,?,?)",(nm,addr,city,pin) )
+            
+            con.commit()
+            msg = "Record successfully added"
+      except:
+         con.rollback()
+         msg = "error in insert operation"
+      
+      finally:
+         return render_template("result.html",msg = msg)
+         con.close()'''
 
 if __name__ == '__main__':
    app.run(debug = True)
